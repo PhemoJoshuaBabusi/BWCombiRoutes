a91b0f8d58096a63ba93714693426337044a0ac8
PhemoJoshuaBabusi <58779464+PhemoJoshuaBabusi@users.noreply.github.com>
2019-12-23 12:34:18 +0200

    Finalish prototype

--- app/src/main/java/com/Np2/bwcombiroutes/MainActivity.java
+++ app/src/main/java/com/Np2/bwcombiroutes/MainActivity.java
@@ -2,12 +2,15 @@ package com.Np2.bwcombiroutes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Spinner;
+
+import static com.Np2.bwcombiroutes.R.mipmap.ic_launcher_round;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,24 +25,35 @@ public class MainActivity extends AppCompatActivity {
 
         routesSearchView = findViewById(R.id.routesSearchView);
         routesImageView = findViewById(R.id.routesImageView);
-        routesSpinner  = findViewById(R.id.routesSpinner);
+        routesSpinner = findViewById(R.id.routesSpinner);
 
-        routesSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.routes, android.R.layout.simple_spinner_dropdown_item);
+
+        routesSpinner.setAdapter(adapter);
+
+        new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                switch (item) {
+                    case "Tlokweng Route 1":
+                        ((ImageView) findViewById(R.id.routesImageView)).setImageResource(
+                                    (ic_launcher_round));
+
+                    case "Tlokweng Route 2":
+                        ((ImageView) findViewById(R.id.routesImageView)).setImageResource(
+                                (R.mipmap.ic_launcher));
+                }
             }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
+            public void onNothingSelected(AdapterView<?> parent) {
 
-        routesImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
             }
-        });
+        };
+
+
     }
 }
---
+++ app/src/main/python/routes.py
@@ -0,0 +1,2 @@
+Routes = ["Tlokweng Routes 1", "Tlokweng Route 2", "Block 8 Route 4"]
+print(Routes)
