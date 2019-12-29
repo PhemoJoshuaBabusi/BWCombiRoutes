a859bbd82f3513aaaea1fd914077219a901213c3
PhemoJoshuaBabusi <58779464+PhemoJoshuaBabusi@users.noreply.github.com>
2019-12-30 00:15:26 +0200

    Finalish prototype

--- app/src/main/java/com/Np2/bwcombiroutes/MainActivity.java
+++ app/src/main/java/com/Np2/bwcombiroutes/MainActivity.java
@@ -11,11 +11,10 @@ import android.widget.SearchView;
 import android.widget.Spinner;
 
 import static com.Np2.bwcombiroutes.R.mipmap.ic_launcher_round;
+import static com.Np2.bwcombiroutes.R.mipmap.tlokweng;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SearchView routesSearchView;
-    private ImageView routesImageView;
     private Spinner routesSpinner;
 
     @Override
@@ -23,37 +22,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        routesSearchView = findViewById(R.id.routesSearchView);
-        routesImageView = findViewById(R.id.routesImageView);
+        SearchView routesSearchView = findViewById(R.id.routesSearchView);
         routesSpinner = findViewById(R.id.routesSpinner);
 
 
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.routes, android.R.layout.simple_spinner_dropdown_item);
 
         routesSpinner.setAdapter(adapter);
 
-        new AdapterView.OnItemSelectedListener() {
-
+        routesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String item = parent.getItemAtPosition(position).toString();
                 switch (item) {
                     case "Tlokweng Route 1":
                         ((ImageView) findViewById(R.id.routesImageView)).setImageResource(
-                                    (ic_launcher_round));
+                                (tlokweng));
+                        break;
 
                     case "Tlokweng Route 2":
                         ((ImageView) findViewById(R.id.routesImageView)).setImageResource(
                                 (R.mipmap.ic_launcher));
+                        break;
+
+                    case "Block 8 Route 4":
+                        ((ImageView) findViewById(R.id.routesImageView)).setImageResource(
+                                (ic_launcher_round));
                 }
             }
 
+            @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
-        };
+        });
 
+        routesSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
-    }
-}
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                routesSpinner.performClick();
+
+                adapter.getFilter().filter(query);
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+        });
+
+    }}
